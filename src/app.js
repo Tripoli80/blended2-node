a18e5a9e5299a53f6b8efc0905b0ce6d9152a5d6
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-11-06 13:41:04 +0200

    setup app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use((req, res, next) => res.sendStatus(240))
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(morgan(formatLogger));
+app.use(cors());
+app.use(express.json());
 
-module.exports = app
+app.get("/admin", (req, res, next) => {
+  const error = new Error("Our error");
+  error.status = 543;
+  next(error);
+});
+
+app.use((_, res) => res.status(404).json({ message: "Not found" }));
+
+app.use((err, _, res, next) => {
+  const { status = 500, message = "Server Internal error" } = err;
+
+  res.status(status).json({ message });
+});
+
+module.exports = app;
